5596f71c3b7461e8a1a93842080cf8023cf960b1
jt00721 <titusgloverj@gmail.com>
2025-01-04 15:06:00 +0000

    Accept user inputted names as command line arguments and print the list of names

--- main.go
+++ main.go
@@ -1,5 +1,20 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"os"
+)
 
+func main() {
+	// Check if more than 2 names are provided as arguments
+	if len(os.Args) < 3 {
+		fmt.Println("Please provide more than one name as arguments.")
+		return
+	}
+
+	// Declare a slice of names from the provided arguments
+	names := os.Args[1:]
+
+	// Print the provided names
+	fmt.Println("Names provided:", names)
 }
