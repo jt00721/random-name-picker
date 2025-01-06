adb867693d1cffa962e2be8794d80cb43fe0fc41
jt00721 <titusgloverj@gmail.com>
2025-01-06 15:11:02 +0000

    Add read names from file functionality and add validation

--- main.go
+++ main.go
@@ -1,26 +1,74 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"math/rand"
 	"os"
 	"time"
 )
 
+// Read names from file logic
+func readNamesFromFile(filename string) ([]string, error) {
+	// Open file for reading
+	file, err := os.Open(filename)
+	if err != nil {
+		return nil, err
+	}
+
+	defer file.Close()
+
+	var names []string
+	// Initialise scanner to read from file
+	scanner := bufio.NewScanner(file)
+	// Scan() will remain true till the end of input (file) is reached or an error occurs
+	for scanner.Scan() {
+		names = append(names, scanner.Text())
+	}
+
+	return names, scanner.Err()
+}
+
 func main() {
-	// Check if no names provided as arguments
+	// Provide usage of program if no names or file is provided as arguments
 	if len(os.Args) < 2 {
-		fmt.Println("No names provided")
+		fmt.Println("Usage:")
+		fmt.Println("  Provide names as arguments: go run main.go Alice Bob Charlie")
+		fmt.Println("  Or provide a file with names: go run main.go -file names.txt")
 		return
 	}
-	// Check if more than 2 names are provided as arguments
-	if len(os.Args) < 3 {
-		fmt.Println("Please provide more than one name as arguments.")
+	// Read names from file provided as argument
+	var names []string
+	if os.Args[1] == "-file" {
+		// Validate file path is provided
+		if len(os.Args) < 3 {
+			fmt.Println("Please provide the file path after -file.")
+			return
+		}
+
+		filepath := os.Args[2]
+		var err error
+		names, err = readNamesFromFile(filepath)
+		if err != nil {
+			fmt.Printf("Error reading file: %v\n", err)
+			return
+		}
+	} else {
+		// Slice of names from the provided arguments
+		names = os.Args[1:]
+	}
+
+	// Check if no names are provided
+	if len(names) == 0 {
+		fmt.Println("No names provided.")
 		return
 	}
 
-	// Declare a slice of names from the provided arguments
-	names := os.Args[1:]
+	// Check if only one name is provided
+	if len(names) == 1 {
+		fmt.Println("Why would you use me for ONE name??... Hmmm... Maybe the name you want is", names[0])
+		return
+	}
 
 	// Print how many names were provided
 	fmt.Println("Number of provided names:", len(names))
